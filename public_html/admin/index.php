6a179ab6ee41863a638fe69cdf66892a6a07af3f
Sanket Bhimani <snk.bhimani.jnd@gmail.com>
2016-08-15 18:04:30 +0530

    security

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -1,4 +1,24 @@
 <?php
-header("location:../index.php");
+session_start();
+
+if(isset($_POST['uname']) && isset($_POST['pass'])){
+	if($_POST['uname']==="admin" && $_POST['pass']==="admin"){
+		$_SESSION['you_can_logged_in']=123;
+		header("location:home.php");
+		die();
+	}
+}
 
 ?>
+<html>
+<body>
+	<form action="bhochu.php" method="post">
+	username: <input type="text" name="uname"><br>
+	password: <input type="password" name="pass"><br>
+	<input type="submit">
+	</form>
+	
+
+
+</body>
+</html>
